33143a6c98e0c7a7ae4980f85753ffcf96f293f2
ChiefChippy2 <chiefchippy2@gmail.com>
2025-08-25 02:08:04 +0200

    Updates and fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,12 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import {defineConfig} from 'eslint/config';
+import google from 'eslint-config-google';
+delete google.rules['valid-jsdoc'];
+delete google.rules['require-jsdoc'];
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {files: ['**/*.{js,mjs,cjs}'], plugins: {js}, extends: ['js/recommended'], languageOptions: {globals: globals.node}},
+  {files: ['**/*.js'], languageOptions: {sourceType: 'commonjs'}},
+  google,
 ]);
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client, ButtonBuilder, ComponentType, ButtonStyle, IntentsBitField, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, MessageFlags, TextDisplayComponent} = require('discord.js');
+const {Client, ButtonBuilder, ButtonStyle, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, MessageFlags} = require('discord.js'); // eslint-disable-line
 require('dotenv').config();
 const text = require('./text.json');
 const {appendFile} = require('fs/promises');
@@ -9,34 +9,36 @@ const Cli = new Client({
 });
 
 
-Cli.login(process.env.TOKEN)
+Cli.login(process.env.TOKEN);
 
 Cli.on('clientReady', ready);
 
 Cli.on('interactionCreate', handleInteraction);
 
 async function ready() {
-  console.log(`${Cli.user.tag} Online!`)
+  console.log(`${Cli.user.tag} Online!`);
+
+  const channel = process.env.CHANNEL_ID;
+  const chn = await Cli.channels.fetch(channel);
+  if (!chn) console.error(`[FATAL] Failed to fetch channel (ID: ${channel})!`);
+
   if (!process.env.INIT) {
     try {
-      await initInteraction()
+      await initInteraction(chn);
     } catch (e) {
-      console.error(e)
-      console.error('[FATAL] Failed to initiate Interaction!')
+      console.error(e);
+      console.error('[FATAL] Failed to initiate Interaction!');
     }
-  }
-  else {
+  } else {
+    // const chn = await Cli.channels.fetch(channel);
     // TBD
   }
 }
 
-async function initInteraction() {
+async function initInteraction(chn) {
   // const guild = process.env.GUILD_ID;
-  const channel = process.env.CHANNEL_ID;
-  const chn = await Cli.channels.fetch(channel);
-  if (!chn) console.error(`[FATAL] Failed to fetch channel (ID: ${channel})!`)
   const row = new ActionRowBuilder();
-  for (let info of text.form) {
+  for (const info of text.form) {
     const [cat, name] = [info.category, info.label];
     const btn = new ButtonBuilder();
     btn.setCustomId(`${prefix}_0_${cat}`);
@@ -44,51 +46,78 @@ async function initInteraction() {
     btn.setStyle(ButtonStyle.Primary);
     row.addComponents(btn);
   }
-  
+
   const msg = text.msg;
-  if(msg.components) msg.components.push(row)
-  else msg.components = [row]
+  if (msg.components) msg.components.push(row);
+  else msg.components = [row];
   await chn.send(msg);
-  await appendFile('.env', '\n\nINIT=1')
-  console.log('[INFO] Boutons créés!')
+  await appendFile('.env', '\n\nINIT=1');
+  console.log('[INFO] Boutons créés!');
 }
 
 /**
- * 
- * @param {import('discord.js').Interaction} interaction 
+ *
+ * @param {import('discord.js').Interaction} interaction
  */
 async function handleInteraction(interaction) {
-  const fieldsNo = text.form.questions.length;
-  const response = [];
-  if (interaction.customId === `${prefix}_1`) {
-    for (let i = 1; i <= fieldsNo; i++) response.push(interaction.fields.getTextInputValue(`Q${i}`))
+  if (interaction.customId.startsWith(`${prefix}_1`)) {
+    await interaction.message?.fetch?.()
+    const type = interaction.customId.split('_').slice(-1)[0];
+    const info = text.form.find((el)=>el.category === type);
+    const fieldsNo = info.questions.length;
+    const response = [];
+    for (let i = 1; i <= fieldsNo; i++) response.push(interaction.fields.getTextInputValue(`Q${i}`));
     console.log(response);
-     // Fetch Apps Script endpoint
-    await interaction.reply({
-      content: 'Reçu!',
+    // Save in case of failure
+    await appendFile('save', JSON.stringify([Date.now(), interaction.user.id, interaction.user.tag, ...response])+'\n');
+    await interaction.deferReply({
+      flags: MessageFlags.Ephemeral
+    })
+    // Fetch Apps Script endpoint
+    if (process.env.APPS_SCRIPT_ENDPOINT_URL) {
+      const send = await fetch(process.env.APPS_SCRIPT_ENDPOINT_URL, {
+        method: 'POST',
+        body: JSON.stringify({
+          'timestamp': Date.now(),
+          'userId': interaction.user.id,
+          'userTag': interaction.user.tag,
+          'fields': response,
+          'userJoinDate': interaction.user.createdAt.getTime(),
+        }),
+        redirect: 'follow',
+      }).then((r)=>r.status);
+      if (send !== 200) {
+        return await interaction.followUp({
+          content: text.errMsg,
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+    }
+    await interaction.followUp({
+      content: text.successMsg,
       flags: MessageFlags.Ephemeral,
     });
   }
   if (!interaction.customId.startsWith(`${prefix}_0`)) {
-    return
+    return;
   }
-  const type = interaction.customId.split('_')[2];
-  const info = text.form.find(el=>el.category === type)
+  const type = interaction.customId.split('_').slice(-1)[0];
+  const info = text.form.find((el)=>el.category === type);
   const modal = new ModalBuilder()
-			.setCustomId(`${prefix}_1`)
-			.setTitle(info.title);
+      .setCustomId(`${prefix}_1_${info.category}`)
+      .setTitle(info.title);
 
-	let id = 1
-  const rows = []
+  let id = 1;
+  const rows = [];
   for (const question of info.questions) {
     const input = new TextInputBuilder();
-    input.setCustomId(`Q${id}`)
-    input.setLabel(question.question)
-    input.setStyle(TextInputStyle[question.style])
-    input.setPlaceholder(question.placeholder)
-    rows.push(new ActionRowBuilder().addComponents(input))
-    id ++
+    input.setCustomId(`Q${id}`);
+    input.setLabel(question.question);
+    input.setStyle(TextInputStyle[question.style]);
+    input.setPlaceholder(question.placeholder);
+    rows.push(new ActionRowBuilder().addComponents(input));
+    id ++;
   }
-		modal.addComponents(...rows);
-		await interaction.showModal(modal);
+  modal.addComponents(...rows);
+  await interaction.showModal(modal);
 }
